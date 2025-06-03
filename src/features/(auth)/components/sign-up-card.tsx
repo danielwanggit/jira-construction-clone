5c21a2c583270033eece54b25c36f705feb5a781
Daniel Wang <dxnny@umich.edu>
2025-06-03 17:58:18 -0500

    finishing auth screens, setting up hono

--- src/features/(auth)/components/sign-up-card.tsx
+++ src/features/(auth)/components/sign-up-card.tsx
@@ -3,7 +3,6 @@ import { FcGoogle } from "react-icons/fc"
 import { FaGithub} from "react-icons/fa"
 import { zodResolver } from  "@hookform/resolvers/zod"
 
-
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
 import {
@@ -14,6 +13,7 @@ import {
     CardTitle
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 import {
     Form,
     FormControl,
@@ -22,32 +22,44 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 import { useForm } from "react-hook-form";
-import Link from "next/link";
+
 
 const formSchema = z.object({
+    name: z.string().trim().min(1,"Required"),
     email: z.string().email(),
-    password: z.string().min(1, "Required"),
+    password: z.string().min(8, "Minimum of 8 characters required"),
 });
 
-export const SignInCard = () => {
+export const SignUpCard = () => {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
+            name: "",
             email: "",
             password: "",
         },
     });
-
+    
     const onSubmit = (values: z.infer<typeof formSchema>) => {
         console.log({ values })
     }
-
+        
     return (
         <Card className="w-full h-full md:w-[487px] border-none shadow-none">
             <CardHeader className="flex items-center justify-center text-center p-7">
                 <CardTitle className="text-2xl">
-                    Welcome Back!
+                    Sign Up
                 </CardTitle>
+                <CardDescription>
+                    By signing up, you agree to our{" "}
+                    <Link href="/privacy">
+                        <span className="text-blue-700">Privacy Policy</span>
+                    </Link>{" "}
+                    and{" "}
+                    <Link href="/terms">
+                        <span className="text-blue-700">Terms of Service</span>
+                    </Link>
+                </CardDescription>
             </CardHeader>
             <div className="px-7">
                 <DottedSeparator/>
@@ -55,6 +67,22 @@ export const SignInCard = () => {
             <CardContent className="p-7">
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                        <FormField 
+                            name="name"
+                            control = {form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                    <Input 
+                                        {...field}
+                                        type = "text"
+                                        placeholder = "Enter your name"
+                                    />  
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
                         <FormField 
                             name="email"
                             control = {form.control}
@@ -79,8 +107,8 @@ export const SignInCard = () => {
                                     <FormControl>
                                     <Input 
                                         {...field}
-                                        type = "email"
-                                        placeholder = "Enter Password"
+                                        type = "password"
+                                        placeholder = "Enter your password"
                                     />  
                                     </FormControl>
                                     <FormMessage />
@@ -121,9 +149,9 @@ export const SignInCard = () => {
             </div>
             <CardContent className="p-7 flex items-center justify-center">
                 <p>
-                    Don't have an account?{" "}
-                    <Link href="/sign-up">
-                        <span className="text-blue-700">Sign Up</span>
+                    Already have an account? {" "}
+                    <Link href="/sign-in">
+                        <span className="text-blue-700">Sign in</span>
                     </Link>
                 </p>
             </CardContent>
